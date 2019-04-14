fc95d806186ef429f2d394919587328baa1f3d8c
codemass <worklette@yandex.ru>
2019-04-14 22:29:38 +0300

    Add models, controllers, cooect to db

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test.Models;
 
 namespace Test.Controllers
 {
     public class HomeController : Controller
     {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
         public ActionResult Index()
         {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
             return View();
         }
 
